56ea165e6f6fcfa934d8d393efae7343fd5fb126
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-27 23:42:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,9 +4,9 @@ def my_collect(collection)
     yield(collection[i])
     i += 1
   end
+end
+
   collection = ["ruby", "javascript", "python", "objective-c"]
 my_collect(collection) do |language|
   language.upcase
 end
-end
-
